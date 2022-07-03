1bb506af86edb6b79bd06dcf7b5dffc4f3f50dfc
Brijesh Bhardwaj <ios.brijesh@gmail.com>
2022-07-03 15:29:40 +0530

    Hello world text added

--- DemoApp1/ViewController.swift
+++ DemoApp1/ViewController.swift
@@ -12,8 +12,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("Welcome to git")
+        
+        callApi()
     }
 
-
+    func callApi(){
+        
+        print("Call Api")
+    }
 }
 
