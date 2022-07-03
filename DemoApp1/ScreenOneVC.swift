13ec3e4d1a0fa561c17351c9bf28bafb22821e4a
Brijesh Bhardwaj <ios.brijesh@gmail.com>
2022-07-03 15:38:22 +0530

    One VC and Launch Screen Added

---
+++ DemoApp1/ScreenOneVC.swift
@@ -0,0 +1,29 @@
+//
+//  ScreenOneVC.swift
+//  DemoApp1
+//
+//  Created by BRIJESH iOS on 03/07/22.
+//
+
+import UIKit
+
+class ScreenOneVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
